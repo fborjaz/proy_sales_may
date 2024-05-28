c6afba7972cceaa2b03b86d1287d70dfb03557b6
Frank Borja <fborjaz@unemi.edu.ec>
2024-05-28 17:13:53 -0500

    Correcciones code

--- proy_sales/settings.py
+++ proy_sales/settings.py
@@ -123,7 +123,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/' # url de archivos estaticos
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)#carpeta fisica de archivos estaticos
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),) #carpeta fisica de archivos estaticos
 MEDIA_ROOT = os.path.join(BASE_DIR,'media') # carpeta fisica de archivos de Imagenes
 MEDIA_URL = '/media/' # url de imagenes
 
--- proy_sales/urls.py
+++ proy_sales/urls.py
@@ -20,6 +20,7 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path,include
 from core import views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',views.home,name='home'),
